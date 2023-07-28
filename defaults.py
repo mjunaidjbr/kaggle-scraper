96cabb17341555ad699957032fc7a833d1ab63f4
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2023-07-28 23:21:44 +0500

    added: new python file support with proxy download

--- defaults.py
+++ defaults.py
@@ -2,8 +2,12 @@
 #class name for the a tags in the page
 A_TAGS_CLASS_NAME = "sc-eGFuAX guSrxL"
 AUTHOR_CLASS_NAME = "sc-hhGHuG sc-gXSCqU iIhleq fyPHTK"
-TITLE_CLASS_NAME = "sc-iAEyYk sc-fsQiph sc-kPaQWy bhyXVy feJEwm eYSRGy"
-SUB_TITLE_CLASS_NAME = "sc-fLQRDB sc-bALXmG sc-hYcICj kPbSkA JDLpp kVIkBz"
-DATASET_DESCRIPTION_CLASS_NAME = "sc-dEsEiM kDEXIv"
-LICENSE_CLASS_NAME = "sc-dKfzgJ sc-hIqOWS sc-cQLFSp jQQULV dclpAt fXJqSS"
-TAGS_CLASS_NAME = "sc-dtyhMI ffAMuj"
+TITLE_CLASS_NAME = "sc-iAEyYk sc-fsQiph sc-jEjhTi bhyXVy feJEwm fUKQMf"
+SUB_TITLE_CLASS_NAME = "sc-fLQRDB sc-bALXmG sc-bJCdhx kPbSkA JDLpp leLvsa"
+DATASET_DESCRIPTION_CLASS_NAME = "sc-USgdl frrls"
+LICENSE_CLASS_NAME = "sc-dKfzgJ sc-hIqOWS sc-gZXmNC jQQULV dclpAt jlZdis"
+TAGS_CLASS_NAME = "sc-jvhpUC Xhakg"
+
+#proxy
+proxy_host = "127.0.0.1"
+proxy_port = 8989
